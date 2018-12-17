5215e294aff07954d40d5b0669d89d6abcdfb20b
Almog <almoggueta1@gmail.com>
2018-12-17 06:51:10 -0800

    more classes

--- Expression.h
+++ Expression.h
@@ -14,13 +14,27 @@ using namespace std;
  * Methods that associate for every expression that exist .
  */
 class Expression {
+private:
+    Expression *leftExp;
+    Expression *rightExp;
 public:
+    Expression(Expression *leftExp, Expression *rightExp);
+
+    Expression *getLeftExp() const;
+
+    void setLeftExp(Expression *leftExp);
+
+    Expression *getRightExp() const;
+
+    void setRightExp(Expression *rightExp);
 
     /* Evaluate the expression using the variable values provided and return the result .
      */
     virtual double calculate(map<double, Var *> symbolTable) = 0;
 
     virtual double assign();
+
+
 };
 
 
