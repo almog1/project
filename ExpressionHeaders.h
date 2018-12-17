63b695cf0511ba0a0900be7faa86574b9753ee16
Almog <almoggueta1@gmail.com>
2018-12-17 08:38:57 -0800

    Expressions

---
+++ ExpressionHeaders.h
@@ -0,0 +1,15 @@
+
+#ifndef PROGECT_EXPRESSIONHEADERS_H
+#define PROGECT_EXPRESSIONHEADERS_H
+
+#include "Minus.h"
+#include "Expression.h"
+#include "Plus.h"
+#include "Pow.h"
+#include "Mult.h"
+#include "Number.h"
+#include "Var.h"
+#include "BinaryExpression.h"
+#include "Div.h"
+
+#endif //PROGECT_EXPRESSIONHEADERS_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include "ExpressionHeaders.h"
+
+using namespace std;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    map<string, double> symbolTebla;
+
+    symbolTebla.insert(pair<string, double>("x", 2));
+    symbolTebla.insert(pair<string, double>("y", 4));
+
+
+    Expression *exp = new Div(new Var("x"), new Var("y"));
+    cout << exp->calculate(symbolTebla) << endl;
     return 0;
 }
