90700c62e9c14c76b191e676365680e447467a5c
Almog <almoggueta1@gmail.com>
2018-12-23 11:12:55 -0800

    Expressions

--- LoopCommand.cpp
+++ LoopCommand.cpp
@@ -3,11 +3,11 @@
 //
 
 #include "LoopCommand.h"
-#include "CommandTable.h"
+//#include "CommandTable.h"
 
 int LoopCommand::setParameters(vector<string> data, int index) {
-    CommandTable* commadTab = CommandTable::getInstance();
-    map<string, Expression*> commandTable = commadTab->getTable();
+//    CommandTable* commadTab = CommandTable::getInstance();
+  //  map<string, Expression*> commandTable = commadTab->getTable();
     //check what is the condition
 
     int newIndex = 4;
